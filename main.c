2f5a170bba4b95c2892fb149928dd9ac370854d8
Pedro Henrique De Jesus Barbosa <pede-jes@c2r2p1.42sp.org.br>
2025-01-22 19:36:54 -0300

    rr and half od rra

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: pede-jes <pede-jes@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/15 16:26:47 by pede-jes          #+#    #+#             */
-/*   Updated: 2025/01/16 16:43:31 by pede-jes         ###   ########.fr       */
+/*   Updated: 2025/01/22 18:55:44 by pede-jes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,38 +19,111 @@ long	ft_atoi(char *string);
 void	ft_sa(long martx[]);
 long	ft_len_array(long martx[]);
 void	ft_print_array(long martx[]);
+void	ft_init_array(long martx[], int argc, char *argv[]);
+void	ft_init_b(long martx[], int argc, char *argv[]);
+void	ft_pb(int number, long martx[]);
+void	ft_r(long martx[]);
+void	ft_rr(long martx[]);
 
 int	main(int argc, char *argv[])
 {
-	long	i;
-	long	j;
 	long	stack_a[argc];
 	long	stack_b[argc];
 
-	i = 1;
-	j = 0;
-	while (j < argc - 1)
-	{
-		stack_a[j] = ft_atoi(argv[i]);
-		i++;
-		j++;
-	}
-	stack_a[j] = '\0';
+	ft_init_array(stack_a, argc, argv);
 	printf("\n----------------------INICIO------------------------\n");
 	ft_print_array(stack_a);
-	ft_sa(stack_a);
+	ft_rr(stack_a);
 	printf("\n---------------------INVERTIDO----------------------\n");
 	ft_print_array(stack_a);
 	return (0);
 }
+void	ft_init_array(long martx[], int argc, char *argv[])
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 1;
+	while (i < argc - 1)
+	{
+		martx[i] = ft_atoi(argv[j]);
+		i++;
+		j++;
+	}
+	martx[i] = '\0';
+}
+void	ft_init_b(long martx[], int argc, char *argv[])
+{
+	int	i;
+
+	i = 0;
+	while (i < argc - 1)
+	{
+		martx[i] = 999999999;
+		i++;
+	}
+	martx[i] = '\0';
+}
+
+void	ft_r(long martx[])
+{
+	long	i;
+	long	temp;
+
+	i = ft_len_array(martx);
+	i--;
+	while (i>0)
+	{
+		temp = martx[i - 1];
+		martx[i- 1] = martx[i];
+		martx[i] = temp;
+		i--;
+	}
+}
+void	ft_rr(long martx[])
+{
+	long	i;
+	long	j;
+	long	temp;
+
+	j = 0;
+	i = ft_len_array(martx);
+	i--;
+	j++;
+	while (i>0)
+	{
+		temp = martx[j + 1];
+		martx[j+ 1] = martx[j];
+		martx[j] = temp;
+		i--;
+		j++;
+	}
+}
+// void	ft_pb(int number, long martx[])
+// {
+// 	int i;
+
+// 	while(martx[i] == 999999999)
+// 	{
+// 		i++;
+// 	}
+// 	if(i > 1)
+// 	{
+// 		while ()
+// 		{
+// 			/* code */
+// 		}
+
+// 	}
+
+// }
 void	ft_print_array(long martx[])
 {
 	long	i;
-	long	len_array;
 
 	i = 0;
-	len_array = ft_len_array(martx);
-	while (martx[i])
+	while (martx[i] != '\0')
 	{
 		printf("%ld\n", martx[i]);
 		i++;
@@ -79,9 +152,7 @@ long	ft_len_array(long martx[])
 	}
 	return (i);
 }
-void	ft_pb(long martx[], long martry[])
-{
-}
+
 long	ft_atoi(char *string)
 {
 	long	number;
