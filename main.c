3cf447708694aae9a18dc620e62769ac5275c81c
Pedro Henrique De Jesus Barbosa <pede-jes@c2r2p1.42sp.org.br>
2025-01-23 14:57:53 -0300

    rra, rrr

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: pede-jes <pede-jes@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/15 16:26:47 by pede-jes          #+#    #+#             */
-/*   Updated: 2025/01/22 18:55:44 by pede-jes         ###   ########.fr       */
+/*   Updated: 2025/01/23 14:27:27 by pede-jes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,22 +20,32 @@ void	ft_sa(long martx[]);
 long	ft_len_array(long martx[]);
 void	ft_print_array(long martx[]);
 void	ft_init_array(long martx[], int argc, char *argv[]);
-void	ft_init_b(long martx[], int argc, char *argv[]);
-void	ft_pb(int number, long martx[]);
+void	ft_init_b(long martx[], int argc);
+void	ft_pb(long martx[], long marty[]);
 void	ft_r(long martx[]);
-void	ft_rr(long martx[]);
+void	ft_rrg(long martx[]);
+long	ft_len_populate(long martx[]);
 
 int	main(int argc, char *argv[])
 {
-	long	stack_a[argc];
-	long	stack_b[argc];
+	long	stack_a[sizeof(argc)];
+	long	stack_b[sizeof(argc)];
 
 	ft_init_array(stack_a, argc, argv);
+	ft_init_b(stack_b, argc);
+	printf("\n------------------------A---------------------------\n");
 	printf("\n----------------------INICIO------------------------\n");
 	ft_print_array(stack_a);
-	ft_rr(stack_a);
-	printf("\n---------------------INVERTIDO----------------------\n");
+	printf("\n------------------------B---------------------------\n");
+	ft_print_array(stack_b);
+	ft_pb(stack_a, stack_b);
+	ft_pb(stack_a, stack_b);
+	ft_pb(stack_a, stack_b);
+	printf("\n------------------------PB--------------------------\n");
+	printf("\n------------------------A---------------------------\n");
 	ft_print_array(stack_a);
+	printf("\n------------------------B---------------------------\n");
+	ft_print_array(stack_b);
 	return (0);
 }
 void	ft_init_array(long martx[], int argc, char *argv[])
@@ -53,7 +63,7 @@ void	ft_init_array(long martx[], int argc, char *argv[])
 	}
 	martx[i] = '\0';
 }
-void	ft_init_b(long martx[], int argc, char *argv[])
+void	ft_init_b(long martx[], int argc)
 {
 	int	i;
 
@@ -73,15 +83,15 @@ void	ft_r(long martx[])
 
 	i = ft_len_array(martx);
 	i--;
-	while (i>0)
+	while (i > 0)
 	{
 		temp = martx[i - 1];
-		martx[i- 1] = martx[i];
+		martx[i - 1] = martx[i];
 		martx[i] = temp;
 		i--;
 	}
 }
-void	ft_rr(long martx[])
+void	ft_rrg(long martx[])
 {
 	long	i;
 	long	j;
@@ -90,34 +100,29 @@ void	ft_rr(long martx[])
 	j = 0;
 	i = ft_len_array(martx);
 	i--;
-	j++;
-	while (i>0)
+	while (i > 0)
 	{
 		temp = martx[j + 1];
-		martx[j+ 1] = martx[j];
+		martx[j + 1] = martx[j];
 		martx[j] = temp;
 		i--;
 		j++;
 	}
 }
-// void	ft_pb(int number, long martx[])
-// {
-// 	int i;
+void	ft_pb(long martx[], long marty[])
+{
+	int	i;
+	int	temp;
+	int	len_marty;
 
-// 	while(martx[i] == 999999999)
-// 	{
-// 		i++;
-// 	}
-// 	if(i > 1)
-// 	{
-// 		while ()
-// 		{
-// 			/* code */
-// 		}
-
-// 	}
-
-// }
+	i = 0;
+	temp = martx[0];
+	len_marty = ft_len_populate(marty);
+	marty[len_marty] = temp;
+	ft_rrg(marty);
+	martx[0] = 999999999;
+	ft_rrg(martx);
+}
 void	ft_print_array(long martx[])
 {
 	long	i;
@@ -147,6 +152,17 @@ long	ft_len_array(long martx[])
 
 	i = 0;
 	while (martx[i])
+	{
+		i++;
+	}
+	return (i);
+}
+long	ft_len_populate(long martx[])
+{
+	long	i;
+
+	i = 0;
+	while (martx[i] && martx[i] != 999999999)
 	{
 		i++;
 	}
